41074a5f4689917c0b2893ecbc31730d5a4ae499
Crablet <a.gryczewski999@gmail.com>
2019-01-22 00:32:43 -0500

    Registered TestCommand.

--- src/main/java/studio/spiderling/iexbot/IEXBot.java
+++ src/main/java/studio/spiderling/iexbot/IEXBot.java
@@ -1,20 +1,23 @@
-package org.crabcraft.iexbot;
+package studio.spiderling.iexbot;
 
 import org.crabcraft.Commandler.CommandRegistry;
 import org.crabcraft.Commandler.FrameworkConfig;
 import org.javacord.api.DiscordApiBuilder;
+import studio.spiderling.iexbot.core.commands.TestCommand;
 
 public class IEXBot {
 
     public static void main(String[] args) {
 
         new DiscordApiBuilder().setToken(FrameworkConfig.getToken()).login().thenAccept(api -> {
-            // Create a bot invite link
-            api.createBotInvite();
 
+            // Grab a bot invite
+            System.out.println("Bot invite: " + api.createBotInvite());
+
+            // Initialise the Commandler registry
             CommandRegistry registry = new CommandRegistry(api);
 
+            registry.registerCommand(new TestCommand());
         });
-
     }
 }
